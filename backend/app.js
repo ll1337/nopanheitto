aa0cb08cd07b527d9b3b474f16e92f3438816c14
Leevi <leevi.ketola@tuni.fi>
2023-08-17 20:45:38 +0300

    add backend, express, puppeteer

---
+++ backend/app.js
@@ -0,0 +1,57 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+const puppeteer = require('puppeteer');
+
+
+(async () => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto('https://www.tuni.fi/opiskelijanopas/opintotiedot/opintojaksot/?year=2023');
+
+  // Set screen size
+  //await page.setViewport({width: 1080, height: 1024});
+  //const links = await page.$$eval('a[href^="/opiskelijanopas/opintotiedot/opintojaksot/otm-acbfc47e-10dc-450f-9580-7152e90acc70?year=2023"]', (elements) => {
+  //      return elements.map((element) => element.textContent);
+  //  });
+
+//   console.log(links);
+  const lamaSelector = '.sc-bdVaJa.sc-1t7n2sp-2.laxuH';
+	await page.waitForSelector(lamaSelector);
+	console.log(lamaSelector);
+	console.log(lamaSelector.textContent);
+
+  // Type into search box
+  //await page.type('.search-box__input', 'automate beyond recorder');
+
+  // Wait and click on first result
+  //const searchResultSelector = '.search-box__link';
+  //await page.waitForSelector(searchResultSelector);
+  //await page.click(searchResultSelector);
+
+  // Locate the full title with a unique string
+  //const textSelector = await page.waitForSelector(
+  //  'text/Customize and automate'
+  //);
+  //const fullTitle = await textSelector?.evaluate(el => el.textContent);
+
+  // Print the full title
+  console.log('jee');
+
+  await browser.close();
+})();
