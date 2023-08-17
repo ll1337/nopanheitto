bd2e7375c5db42ec09ba3b882d1b59115017337f
Oona Saksman <oona.saksman@tuni.fi>
2023-08-17 21:03:38 +0300

    Implementoi neliö joka pyörii joka toisella klikkausella

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import { StrictMode } from 'react';
+import { motion } from "framer-motion";
+import {useState} from 'react';
 
 function App() {
+
+  const [diePosition, setDiePosition] = useState(0);
+
+  const dieOn = () => {
+    setDiePosition(diePosition === 0 ? 270 : 0)
+    console.log("on");
+  }
+
   return (
     <StrictMode>
       <div className="App">
@@ -11,6 +20,17 @@ function App() {
             Nopanheitto
           
         </header>
+
+        <motion.div
+          className='motiondiv'
+          onTap={dieOn}
+          animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, diePosition, diePosition, 0],
+            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+          }}
+        />
+        
       </div>
     </StrictMode>
   );
