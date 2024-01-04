221386e9dff95cd77ce83aceeebfb46937e973a9
Oona Saksman <oona.saksman@tuni.fi>
2024-01-04 19:04:24 +0200

    Resize dice and clean up animation.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,10 @@
 import './App.css';
 import { StrictMode } from 'react';
-import { motion } from "framer-motion";
-import {useState} from 'react';
 import DiceAnimation from './DiceAnimation';
 import { Canvas } from 'react-three-fiber';
 
 
 function App() {
-
-  const [diePosition, setDiePosition] = useState(0);
-  const [DisplayResults, setDisplayResults] = useState(true);
-
-
-
-  const dieOn = () => {
-    setDiePosition(diePosition + 270);
-    setDiePosition(diePosition - 270);
-    console.log(DisplayResults);
-    setTimeout(() => {
-      setDisplayResults(false);
-    }, 900);
-  }
 
   return (
     <StrictMode>
@@ -34,6 +18,7 @@ function App() {
         
         <div className='flex'>
         <Canvas>
+          
           <DiceAnimation />
         </Canvas>
 
--- frontend/src/DiceAnimation.js
+++ frontend/src/DiceAnimation.js
@@ -1,55 +1,46 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useFrame } from 'react-three-fiber';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 
+//group.current.scale.set(1, 1, 1);
 
 const DiceAnimation = () => {
   const group = useRef();
   const [isSpinning, setSpinning] = useState(false);
 
   // Load the dice model
-  const gltfLoader = new GLTFLoader();
-  gltfLoader.load('untitled.gltf', (gltf) => {
-    group.current.add(gltf.scene);
-  });
+  useEffect(() => {
+    const gltfLoader = new GLTFLoader();
 
-  // Animation logic using the useFrame hook
-  // useFrame((state, delta) => {
-  //   // Your animation logic here
-  //   // group.current.rotation.x += 0.01;
-  //   // group.current.rotation.y += 0.01;
-  //   group.current.scale.set(1, 1, 1); 
-  // });
+    gltfLoader.load('dice.gltf', (gltf) => {
+      group.current.add(gltf.scene);
+    });
 
-  // const handleDiceClick = () => {
-  //   console.log('Dice clicked!');
-  //   // Add your logic for handling the dice click event
-  // };
+  }, [])
+  
 
 
   useFrame((state, delta) => {
     if (isSpinning) {
-      // group.current.rotation.x += 1;
-      // group.current.rotation.y += 1;
       group.current.rotation.z += 0.1;
-      console.log(group.current.rotation.z);
+      group.current.rotation.y += 0.1;
+    
     }
   });
 
   const handleDiceClick = () => {
     setSpinning(true);
 
-    // Stop spinning after 2 seconds
+    // Stop spinning after about 2 seconds
     setTimeout(() => {
       setSpinning(false);
+      group.current.rotation.set(0, 0, 0);
     }, 2140);
   }
 
   return(
     <mesh>
 
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
       <group ref={group} onClick={handleDiceClick}/>
 
     </mesh>
