0e3f4e67a6c22f1a46abbc00a0801a427566527a
ll1337 <leevi.ketola@gmail.com>
2024-01-31 16:01:27 +0200

    scrape the course units by type

--- backend/scraper.js
+++ backend/scraper.js
@@ -16,55 +16,54 @@ const fs = require('fs');
     await page.setViewport({width: 1080, height: 1024});
 
     await page.goto('https://www.tuni.fi/opiskelijanopas/opintotiedot/opintojaksot/?year=2023&studyLevel=basic-studies&size=50');
+    //await page.goto('https://www.tuni.fi/opiskelijanopas/opintotiedot/opintojaksot/?year=2023&size=50');
 
     // lataa sivulle lisää tuloksia
     const moreSelector ='.sc-bdVaJa.sc-EHOje.sc-13iuobc-0.bgjxSN';
 
+    // Itse kurssien laatikot, 2 erilaista väristä riippuen
+    //const courseSelector = ".sc-bdVaJa sc-135i1ze-0.curriculum-search-result.iHLkz"
+    //const courseSelectorDark = ".sc-bdVaJa.sc-135i1ze-0.curriculum-search-result.giFiGk"
+
     // kurssin nimi, sama <a> sisältää myös linkin kurssiin hrefis
-    const courseSelector = '.sc-bdVaJa.sc-1t7n2sp-2.laxuH';
+    //const courseNameSelector = '.sc-bdVaJa.sc-1t7n2sp-2.laxuH';
 
     // kurssin koodi, textContentilla saa
-    const codeSelector = '.sc-bdVaJa.sc-8sw1nw-0.eLXybm'
+    //const codeSelector = '.sc-bdVaJa.sc-8sw1nw-0.eLXybm';
+    //const codeSelectorDifferent = 'sc-bdVaJa sc-8sw1nw-0 eLXybm';
 
     // "oLeT pÄäSsYt LiStAn LoPpUun"
-    const eofSelector = '.sc-bdVaJa.sc-13iuobc-1.jnhQcf'
+    const eofSelector = '.sc-bdVaJa.sc-13iuobc-1.jnhQcf';
 
     const defaultTimeout = 10000;
 
     let courses;
     
     while (true) {
-        await page.waitForSelector(courseSelector);
-        //console.log(Array.from(document.querySelectorAll(codeSelector)))
         console.log('looppi');
         // getAttribute('href');
+
 
         const eof = await page.$(eofSelector);
 
         if (eof) {
             console.log('loppuun päästiin gg');
-            const elements = await page.$$eval(courseSelector, (elements) => {
-                return elements.map((element) => element.textContent);
+            //const elements = await page.$$eval(courseNameSelector, (elements) => {
+                //return elements.map((element) => element.textContent);
+            //});
+            const result = await page.evaluate(() => {
+                let courseUnitDivs = document.querySelectorAll('div[type="course-units"]');
+                let textContentsArray = [];
+
+                courseUnitDivs.forEach(function(element) {
+                    textContentsArray.push(element.textContent);
+                });
+                return textContentsArray;
             });
-            courses = elements;
+            courses = result;
             break;
         }
 
-        //new Promise(r => setTimeout(r, defaultTimeout));
-        //await page.waitForTimeout(defaultTimeout);
-
-        //await page.waitForFunction(
-        //    (targetClassName, expectedChildCount) => {
-            // Find the target element by class name
-        //    const targetElement = document.querySelector(`.${targetClassName}`);
-
-            // Check if the target element exists and has the expected number of children
-        //    return targetElement && targetElement.children.length >= expectedChildCount;
-        //    },
-        //    {},
-            //targetClassName,
-        //    3 // Replace 3 with the desired number of children
-        //);
         await page.waitForSelector(moreSelector, {visible:true, timeout:5000});
 
         const moreButton = await page.$(moreSelector);
