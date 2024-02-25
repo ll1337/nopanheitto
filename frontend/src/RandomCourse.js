c7a0c3b6f2cd210164cac0f714062053abd2393d
Oona Saksman <oona.saksman@tuni.fi>
2024-02-25 15:34:19 +0200

    Fetch random course from backend and display it after roll

--- frontend/src/RandomCourse.js
+++ frontend/src/RandomCourse.js
@@ -2,20 +2,50 @@ import "./RandomCourse.css";
 import React, { useState } from "react";
 
 function RandomCourse({ onClick }) {
-  const [course, setCourse] = useState("Heitä noppaa!");
+  const [course, setCourse] = useState(null);
   const [buttonVisible, setButtonVisible] = useState(true);
 
-  const handleButtonClick = () => {
+  const handleButtonClick = async () => {
     onClick();
     setButtonVisible(false);
-    setCourse("Vektorit ja matriisit? :) <3 ");
+
+    try {
+      const response = await fetch('http://localhost:3000/getRandomCourse');
+      if (!response.ok) {
+        throw new Error('Failed to fetch course');
+      }
+      const data = await response.json();
+      setCourse(data);
+    } catch (error) {
+      console.error('Error fetching course:', error.message);
+    }
+
     setTimeout(() => {
       setButtonVisible(true);
     }, 2180);
   };
+
   return (
     <div onClick={handleButtonClick} className="result">
-      {buttonVisible && <div className="course">{course}</div>}
+      {buttonVisible && (
+        <div className="course">
+          {course ? (
+            Object.keys(course).map(courseCode => (
+              <div key={courseCode}>
+                <p>{courseCode}</p>
+                <p>
+                  {" "}
+                  <a href={`https://www.tuni.fi${course[courseCode].courseLink}`} target="_blank" rel="noopener noreferrer">
+                    {course[courseCode].courseName}
+                  </a>
+                </p>
+              </div>
+            ))
+          ) : (
+            "Heitä noppaa!"
+          )}
+        </div>
+      )}
     </div>
   );
 }
